30680cdd7519bc6b17c4f2ab5ee18461997c3c63
Josepa <yogesh.josepa393@gmail.com>
2018-03-02 08:12:44 -0600

    Union de ambos

---
+++ Progra analisis/Progra analisis/Adaptability.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Progra_analisis
+{
+    class Adaptability
+    {
+        private int distanceRGBHistogram;
+        private int distanceGradientHistogram;
+        private int distanceAverage;
+
+        private int manhattan(ArrayList individual)
+        {
+            int distanceValue = 0;
+
+            for (int i = 0; i < Image.finalImage.getHistogramRGB().Count; i++)
+            {
+                distanceValue += Math.Abs((int)Image.finalImage.getHistogramRGB()[i] - (int)individual[i]);
+            }
+            return distanceValue;
+        }
+
+        private void setDistanceRGBHistogram(ArrayList RGBHistogram)
+        {
+            distanceRGBHistogram = manhattan(RGBHistogram);
+        }
+
+        private void setDistanceGradientHistogram(ArrayList gradientHistogram)
+        {
+            distanceGradientHistogram = 0; //Function for gradient distance
+        }
+
+        private void setDistanceAverage()
+        {
+            distanceAverage = (distanceRGBHistogram + distanceGradientHistogram) / 2;
+        }
+
+        public Adaptability(ArrayList RGBHistogram, ArrayList gradientHistogram)
+        {
+            setDistanceRGBHistogram(RGBHistogram);
+            setDistanceGradientHistogram(gradientHistogram);
+            setDistanceAverage();
+        }
+
+        //adaptabilityOperation is how the adaptability is calculated.
+        //1 for the avarege between the different distances.
+        public int getAdaptability(int adaptabilityOperation)
+        {
+            switch (adaptabilityOperation)
+            {
+                case 1:
+                    return distanceAverage;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ Progra analisis/Progra analisis/Sort.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra_analisis
+{
+    class Sort
+    {
+        //Private Methods for Merge Sort
+        private static List<Image> merge(List<Image> leftImages, List<Image> rightImages)
+        {
+            List<Image> result = new List<Image>(leftImages.Count + rightImages.Count);
+            int indexLeftImages = 0;
+            int indexRightImages = 0;
+            int indexResult = 0;
+
+            while (indexLeftImages < leftImages.Count || indexRightImages < rightImages.Count)
+            {
+                if (indexLeftImages < leftImages.Count && indexRightImages < rightImages.Count)
+                {
+                    if (leftImages[indexLeftImages].getAdaptability(1) <= rightImages[indexRightImages].getAdaptability(1)) //Comparison Ascendent or Descendent
+                    {
+                        result[indexResult] = leftImages[indexLeftImages];
+                        indexLeftImages++;
+                        indexResult++;
+                    }
+                    else
+                    {
+                        result[indexResult] = rightImages[indexRightImages];
+                        indexRightImages++;
+                        indexResult++;
+                    }
+                }
+                else
+                {
+                    if (indexLeftImages < leftImages.Count)
+                    {
+                        result[indexResult] = leftImages[indexLeftImages];
+                        indexLeftImages++;
+                        indexResult++;
+                    }
+                    else
+                    {
+                        if (indexRightImages < rightImages.Count)
+                        {
+                            result[indexResult] = rightImages[indexRightImages];
+                            indexRightImages++;
+                            indexResult++;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        //Private Methods for Quick Sort
+        private static void swapValues(List<Image> images, int indexA, int indexB)
+        {
+            Image temp = images[indexA];
+            images[indexA] = images[indexB];
+            images[indexB] = temp;
+        }
+
+        private static int partitionList(List<Image> images, int left, int right, Image pivot)
+        {
+            int leftPointer = left - 1;
+            int rightPointer = right;
+
+            while (true)
+            {
+                while (images[++leftPointer].getAdaptability(1) < pivot.getAdaptability(1)) ; //Comparison Ascendent or Descendent
+
+                while (rightPointer > 0 && images[--rightPointer].getAdaptability(1) > pivot.getAdaptability(1)) ; //Comparison Ascendent or Descendent
+
+                if (leftPointer >= rightPointer)
+                {
+                    break;
+                }
+                else
+                {
+                    swapValues(images, leftPointer, rightPointer);
+                }
+            }
+
+            swapValues(images, leftPointer, right);
+            return leftPointer;
+        }
+
+        public static void quickSort(List<Image> images, int left, int right)
+        {
+            if (right - left <= 0)
+            {
+                return; //Base Case
+            }
+            else
+            {
+                Image pivot = images[right];
+                int pivotLocation = partitionList(images, left, right, pivot);
+
+                quickSort(images, left, pivotLocation - 1);
+
+                quickSort(images, pivotLocation + 1, right);
+            }
+        }
+
+        public static List<Image> mergeSort(List<Image> images)
+        {
+            if (images.Count <= 1)
+            {
+                return images; //Base Case
+            }
+
+            int center = images.Count / 2;
+            List<Image> leftImages = new List<Image>(center);
+            List<Image> rightImages;
+
+            if (images.Count % 2 == 0)
+            {
+                rightImages = new List<Image>(center);
+            }
+            else
+            {
+                rightImages = new List<Image>(center + 1);
+            }
+
+            List<Image> result = new List<Image>(images.Count);
+
+            for (int i = 0; i < center; i++)
+            {
+                leftImages[i] = images[i];
+            }
+
+            int c = 0;
+            for (int j = center; j < images.Count; j++)
+            {
+                if (c != images.Count)
+                {
+                    rightImages[c] = images[j];
+                    c++;
+                }
+            }
+
+            leftImages = mergeSort(leftImages);
+            rightImages = mergeSort(rightImages);
+
+            result = merge(leftImages, rightImages);
+
+            return result;
+        }
+    }
+}
