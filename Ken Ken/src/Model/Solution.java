1eae558065149943f5feba7ea4b0a96ca5279f3f
richbm10 <ricardobonilla99@hotmail.com>
2018-04-18 18:44:13 -0600

    TOME E E E EEEEE

--- Ken Ken/src/Model/Solution.java
+++ Ken Ken/src/Model/Solution.java
@@ -10,10 +10,64 @@ package Model;
  * @author jose pablo
  */
 public class Solution {
-     private KenKen kenken;
+    private NodekenKen matrix[][];
+    private boolean failure = false;
+    private int shapeType;
+    private int[] permutation;
 
-     public Solution(KenKen pKenken){
-         kenken = pKenken;
-     }
+    private boolean podeByShape(){
+        //Validar por tipo de forma que no se puedan repetir 2 numeros en la permutacion
+    }
+    
+    private boolean podeByRowColumn(){
+        if(checkRow() == false || checkColumn() == false){
+            return false;
+        }
+        return true;
+    }
+    
+    private boolean checkRow(){
+    
+    }
+    
+    private boolean checkColumn(){
+    
+    }
+    
+    //It copies the previous matrix that was updated with a new permutation
+    //Revisar
+    private void copyMatrix(NodekenKen[][] pastMatrix){
+        for(int i = 0; i < pastMatrix.length; i++){
+            for(int j = 0; j < pastMatrix[i].length; j++){
+                NodekenKen copiedNode = new NodekenKen();
+            }
+        }
+    }
+    
+    public Solution(){
+        failure = true;
+    }
      
+    public Solution(NodekenKen pMatrix[][]){
+        matrix = pMatrix;
+    }
+     
+    public Solution(Solution solution, int pShapeType, int[] section, int[] pPermutation){
+        copyMatrix(solution.getMatrix());
+        shapeType = pShapeType;
+        permutation = pPermutation;
+    }
+     
+    public boolean isFailure(){
+        return failure;
+    }
+    
+    public boolean isPromising(){
+        podeByShape();
+        podeByRowColumn();
+    }
+    
+    public NodekenKen[][] getMatrix(){
+        return matrix;
+    }
 }
